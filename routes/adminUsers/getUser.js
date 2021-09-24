7085d08733d68c161269f6992bccfa6d3a140966
dilumharshana <dilum.harshana123@gmail.com>
2021-09-25 01:52:16 +0530

    get user modifications

--- routes/adminUsers/getUser.js
+++ routes/adminUsers/getUser.js
@@ -6,7 +6,11 @@ const user = require("../../models/userModel");
 const getUser = async (req, res) => {
   try {
     const { id } = jwt_decode(req.params.id);
+
+    //getting token and convert
     const isUserExists = await user.findOne({ _id: id });
+
+    //validate token
     if (isUserExists) {
       if (isUserExists._doc.isAccountActivated) {
         const { password, ...other } = isUserExists._doc;
@@ -14,7 +18,10 @@ const getUser = async (req, res) => {
       }
       return res.status(500).json("your account is not activated !");
     }
-    res.status(404).json("User not found");
+
+    console.log("invalid");
+    //if token is invalid
+    return res.status(404).json("User not found");
   } catch (error) {
     return res.status(500).json(error);
   }
